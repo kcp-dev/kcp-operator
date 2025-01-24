3b5729ca82003b906314d07e6f9553c3dbf416bf
Christoph Mewes <christoph@kubermatic.com>
2025-01-24 18:30:46 +0100

    remove unneeded code
    On-behalf-of: @SAP christoph.mewes@sap.com

--- internal/kubernetes/metadata.go
+++ internal/kubernetes/metadata.go
@@ -20,35 +20,7 @@ import (
 	"maps"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/sets"
 )
-
-// HasFinalizer tells if a object has all the given finalizers.
-func HasFinalizer(o metav1.Object, names ...string) bool {
-	return sets.New(o.GetFinalizers()...).HasAll(names...)
-}
-
-func HasAnyFinalizer(o metav1.Object, names ...string) bool {
-	return sets.New(o.GetFinalizers()...).HasAny(names...)
-}
-
-// HasOnlyFinalizer tells if an object has only the given finalizer(s).
-func HasOnlyFinalizer(o metav1.Object, names ...string) bool {
-	return sets.New(o.GetFinalizers()...).Equal(sets.New(names...))
-}
-
-// HasFinalizerSuperset tells if the given finalizer(s) are a superset
-// of the actual finalizers.
-func HasFinalizerSuperset(o metav1.Object, names ...string) bool {
-	return sets.New(names...).IsSuperset(sets.New(o.GetFinalizers()...))
-}
-
-// RemoveFinalizer removes the given finalizers from the object.
-func RemoveFinalizer(obj metav1.Object, toRemove ...string) {
-	set := sets.New(obj.GetFinalizers()...)
-	set.Delete(toRemove...)
-	obj.SetFinalizers(sets.List(set))
-}
 
 func EnsureLabels(o metav1.Object, toEnsure map[string]string) {
 	labels := maps.Clone(o.GetLabels())
