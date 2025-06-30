62142e8f5a7461b92d9023094ab67789d3f34bf3
Mangirdas Judeikis <Mangirdas@Judeikis.LT>
2025-06-30 21:13:53 +0300

    Drop auth config from shards

--- sdk/applyconfiguration/operator/v1alpha1/shardspec.go
+++ sdk/applyconfiguration/operator/v1alpha1/shardspec.go
@@ -117,14 +117,6 @@ func (b *ShardSpecApplyConfiguration) WithDeploymentTemplate(value *DeploymentTe
 	return b
 }
 
-// WithAuth sets the Auth field in the declarative configuration to the given value
-// and returns the receiver, so that objects can be built by chaining "With" function invocations.
-// If called multiple times, the Auth field is set to the value of the last call.
-func (b *ShardSpecApplyConfiguration) WithAuth(value *AuthSpecApplyConfiguration) *ShardSpecApplyConfiguration {
-	b.Auth = value
-	return b
-}
-
 // WithRootShard sets the RootShard field in the declarative configuration to the given value
 // and returns the receiver, so that objects can be built by chaining "With" function invocations.
 // If called multiple times, the RootShard field is set to the value of the last call.
