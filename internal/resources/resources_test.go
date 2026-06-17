d1866b3a6c1cae877a654a45a7d2e193f0d17eb0
Mangirdas Judeikis <mangirdas@judeikis.lt>
2026-06-17 10:17:12 +0300

    lint

--- internal/resources/resources_test.go
+++ internal/resources/resources_test.go
@@ -147,7 +147,7 @@ func requiredModuleVersion(module string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	defer file.Close()
+	defer func() { _ = file.Close() }()
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
