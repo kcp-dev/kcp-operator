2766ac8f75a8eb03a59c3ea9507043e63231cfa5
Mangirdas Judeikis <mangirdas@judeikis.lt>
2026-09-15 14:08:31 +0300

    Bump kcp to 0.33.0

--- internal/resources/resources.go
+++ internal/resources/resources.go
@@ -44,7 +44,7 @@ const (
 	//
 	// When changing this to a new minor version, you must also update
 	// the .prow.yaml accordingly and shift the jobs.
-	ImageTag = "v0.32.3"
+	ImageTag = "v0.33.0"
 
 	appNameLabel      = "app.kubernetes.io/name"
 	appInstanceLabel  = "app.kubernetes.io/instance"
