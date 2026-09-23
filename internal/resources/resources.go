dc130c8644957a9cf872ce0caa497b8745ff9978
Mangirdas Judeikis <mangirdas@judeikis.lt>
2026-09-23 17:08:56 +0300

    [release-0.9] bump kcp to 0.32.5 (#300)
    * bump kcp to 0.32.5
    
    * Fix Upgrade path

--- internal/resources/resources.go
+++ internal/resources/resources.go
@@ -45,7 +45,7 @@ const (
 	//
 	// When changing this to a new minor version, you must also update
 	// the .prow.yaml accordingly and shift the jobs.
-	ImageTag = "v0.32.3"
+	ImageTag = "v0.32.5"
 
 	appNameLabel      = "app.kubernetes.io/name"
 	appInstanceLabel  = "app.kubernetes.io/instance"
